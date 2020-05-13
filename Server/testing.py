d0b9c4dab6d39cd6494ba5c0d6f56267bbcb806c
MichelKrispin <michel.krispin@gmail.com>
2020-05-13 10:34:50 +0200

    Improves testing

--- Server/testing.py
+++ Server/testing.py
@@ -3,9 +3,11 @@
 from ChessValidator import ChessValidator
 from contextlib import contextmanager
 import sys, os
+import inspect
 
 # --------------------
 # For suppressing the output of the functions which are tested
+# (copied from stackoverflow)
 @contextmanager
 def suppress_stdout():
     with open(os.devnull, "w") as devnull:
@@ -15,6 +17,73 @@ def suppress_stdout():
             yield
         finally:
             sys.stdout = old_stdout
+# ------------------
+
+# ------------------
+# The line number to show where something failed
+def line_number():
+    """Returns the current line number."""
+    return inspect.currentframe().f_back.f_lineno
+# ------------------
+
+# ------------------
+# A helper function
+def generate_board_from_string(string):
+    """
+    A helper function to generate a board from a
+    string so there is no need to always state the
+    mirrored dictionary.
+    The string should be given as:
+    8|R N B Q K B N R|
+    7|P P P P P P P P|
+    6|               |
+    5|               |
+    4|               |
+    3|               |
+    2|p p p p p p p p|
+    1|r n b q k b n r|
+
+    -> Linue numbers for readability
+    -> | to indicate start and end
+    -> Spaces in between
+    -> Spaces for empty spots
+    """
+    board = {
+        'a': ['', '', '', '', '', '', '', ''],
+        'b': ['', '', '', '', '', '', '', ''],
+        'c': ['', '', '', '', '', '', '', ''],
+        'd': ['', '', '', '', '', '', '', ''],
+        'e': ['', '', '', '', '', '', '', ''],
+        'f': ['', '', '', '', '', '', '', ''],
+        'g': ['', '', '', '', '', '', '', ''],
+        'h': ['', '', '', '', '', '', '', ''],
+    }
+    columns = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    col_index = 0
+    row_index = 7
+    i = 0
+    while i < len(string):
+        if string[i] not in ['r', 'R', 'n', 'N', 'b', 'B', 'q', 'Q', 'k', 'K', 'p', 'P', ' ']:
+            i += 1
+            continue
+
+        try:
+            board[columns[col_index]][row_index] = string[i]
+            i += 1
+        except IndexError:
+            print(f'Index error using col_index: {col_index} and row_index: {row_index}')
+            return
+        # Skip next element and go on
+        i += 1
+
+        col_index += 1
+        if col_index > 7:
+            col_index = 0
+            row_index -= 1
+        if row_index < 0:
+            break
+
+    return board
 # ------------------
 
 """
@@ -32,22 +101,25 @@ REMINDER
   a b c d e f g h
 """
 
+# ---------------------------
+# Testing generic moves 
+# ---------------------------
 def testing_chess_validator_move_generic():
     """
     Testing generic stuff such as inputting same position or same team.
     """
     result = ['>> Testing Generic']
     chess_validator = ChessValidator()
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', 'p', ' ', ' ', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-    }
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|               |
+3|               |
+2|p p p p p p p p|
+1|r n b q k b n r|
+""")
     
     from_string = 'a1'
     to_string = 'a1'
@@ -56,7 +128,8 @@ def testing_chess_validator_move_generic():
         result.append(test_result(
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
-            'Same position and same team'))
+            'Same position and same team',
+            line_number()))
 
     # -----
 
@@ -68,7 +141,8 @@ def testing_chess_validator_move_generic():
         result.append(test_result(
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
-            'Same team'))
+            'Same team',
+            line_number()))
 
     # -----
 
@@ -79,27 +153,30 @@ def testing_chess_validator_move_generic():
         result.append(test_result(
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
-            'Different team'))
+            'Different team',
+            line_number()))
 
     # -----
     
     result.append('> Finished')
     return result
 
-
+# ---------------------------
+# Testing the rook
+# ---------------------------
 def testing_chess_validator_move_rook():
     result = ['>> Testing Move Rook']
     chess_validator = ChessValidator()
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', 'p', ' ', ' ', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-    }
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|               |
+3|               |
+2|p p p p p p p p|
+1|r n b q k b n r|
+""")
     
     from_string = 'a1'
     to_string = 'a3'
@@ -108,20 +185,21 @@ def testing_chess_validator_move_rook():
         result.append(test_result(
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
-            'Drawing across near pawn'))
+            'Drawing across near pawn',
+            line_number()))
 
     # ------
 
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', 'p', ' ', ' ', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': ['r', ' ', ' ', 'p', ' ', ' ', 'P', 'R'],
-    }
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|p              |
+3|               |
+2|  p p p p p p p|
+1|r n b q k b n r|
+""")
     
     from_string = 'a1'
     to_string = 'a5'
@@ -131,21 +209,21 @@ def testing_chess_validator_move_rook():
         result.append(test_result(
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
-            'Drawing across 3 step away pawn'))
+            'Drawing across 3 step away pawn',
+            line_number()))
         
     # -----
 
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', 'p', ' ', ' ', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': ['r', ' ', ' ', 'p', ' ', ' ', 'P', 'R'],
-    }
-    
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|p              |
+3|               |
+2|  p p p p p p p|
+1|r n b q k b n r|
+""")
     from_string = 'a1'
     to_string = 'a3'
     active_player = 0
@@ -155,20 +233,21 @@ def testing_chess_validator_move_rook():
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
             'Drawing two steps',
+            line_number(),
             False))
         
     # -----
 
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', 'p', ' ', ' ', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': ['r', 'p', ' ', 'P', ' ', ' ', ' ', 'R'],
-    }
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|  P P P P P P P|
+6|               |
+5|               |
+4|P              |
+3|               |
+2|p p p p p p p p|
+1|r n b q k b n r|
+""")
     
     from_string = 'a8'
     to_string = 'a5'
@@ -179,20 +258,21 @@ def testing_chess_validator_move_rook():
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
             'Drawing two steps (black team)',
+            line_number(),
             False))
         
     # -----
 
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', 'p', ' ', ' ', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': [' ', 'p', ' ', 'r', ' ', ' ', 'P', 'R'],
-    }
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|r              |
+3|               |
+2|p p p p p p p p|
+1|  n b q k b n r|
+""")
     
     from_string = 'a4'
     to_string = 'g4'
@@ -203,20 +283,21 @@ def testing_chess_validator_move_rook():
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
             'Drawing sideways',
+            line_number(),
             False))
         
     # -----
 
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', ' ', ' ', 'p', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': [' ', 'p', ' ', 'r', ' ', ' ', 'P', 'R'],
-    }
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|r     p        |
+3|               |
+2|p p p   p p p p|
+1|  n b q k b n r|
+""")
     
     from_string = 'a4'
     to_string = 'g4'
@@ -226,20 +307,21 @@ def testing_chess_validator_move_rook():
         result.append(test_result(
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
-            'Drawing sideways across pawn'))
+            'Drawing sideways across pawn',
+            line_number()))
         
     # -----
 
-    board = {
-        'h': ['r', 'p', ' ', ' ', ' ', ' ', 'P', 'R'],
-        'g': ['n', 'p', ' ', 'p', ' ', ' ', 'P', 'N'],
-        'f': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'e': ['q', 'p', ' ', ' ', ' ', ' ', 'P', 'Q'],
-        'd': ['k', ' ', ' ', ' ', ' ', ' ', 'P', 'K'],
-        'c': ['b', 'p', ' ', ' ', ' ', ' ', 'P', 'B'],
-        'b': ['n', 'p', ' ', ' ', ' ', ' ', 'P', 'N'],
-        'a': [' ', 'p', ' ', 'r', ' ', ' ', 'P', 'R'],
-    }
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|r             p|
+3|               |
+2|p p p p p p p  |
+1|  n b q k b n r|
+""")
     
     from_string = 'a4'
     to_string = 'f4'
@@ -250,6 +332,7 @@ def testing_chess_validator_move_rook():
             chess_validator.validate_move(
             board, from_string, to_string, active_player),
             'Drawing sideways in front of pawn',
+            line_number(),
             False))
         
     # -----
@@ -257,7 +340,76 @@ def testing_chess_validator_move_rook():
     result.append('> Finished')
     return result
 
-def test_result(result, out_message, success_on_false=True):
+# ---------------------------
+# Testing the bishop
+# ---------------------------
+def testing_chess_validator_move_bishop():
+    result = ['>> Testing Move Bishop']
+    chess_validator = ChessValidator()
+
+    # ------
+
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|               |
+3|               |
+2|p p p p p p p p|
+1|r n b q k b n r|
+""")
+    
+    from_string = 'c1'
+    to_string = 'c5'
+    active_player = 0
+    with suppress_stdout():
+        result.append(test_result(
+            chess_validator.validate_move(
+            board, from_string, to_string, active_player),
+            'Drawing straight',
+            line_number()))
+
+    # ------
+
+    result.append('> Finished')
+    return result
+
+'''
+def testing_chess_validator_template():
+    result = ['>> Testing Move Rook']
+    chess_validator = ChessValidator()
+
+    # ------
+
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|               |
+3|               |
+2|p p p p p p p p|
+1|r n b q k b n r|
+""")
+    
+    from_string = 'a1'
+    to_string = 'a3'
+    active_player = 0
+    with suppress_stdout():
+        result.append(test_result(
+            chess_validator.validate_move(
+            board, from_string, to_string, active_player),
+            'Drawing across near pawn',
+            line_number()))
+
+    # ------
+
+    result.append('> Finished')
+    return result
+'''
+
+def test_result(result, out_message, line_number, success_on_false=True):
     """
     The result of the testing function should be put into the result
     argument, the message which describes this test to out_message
@@ -269,12 +421,12 @@ def test_result(result, out_message, success_on_false=True):
         result = not result
     if not result:
         return ('+ ' + out_message)
-    return ('- FAILED - ' + out_message)
+    return ('- FAILED - ' + str(line_number) + ' - ' + out_message)
     
 
 def run_test(testing_function):
     """
-    Run a test and print its results
+    Run a test and print its results.
     """
     result = testing_function()
     for r in result:
@@ -284,3 +436,5 @@ def run_test(testing_function):
 if __name__ == '__main__':
     run_test(testing_chess_validator_move_generic)
     run_test(testing_chess_validator_move_rook)
+    run_test(testing_chess_validator_move_bishop)
+
