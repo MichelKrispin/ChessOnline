7998fe305cb980d26d09ca39d3e1f484a4d19792
MichelKrispin <michel.krispin@gmail.com>
2020-05-13 13:23:10 +0200

    Added test cases

--- Server/testing.py
+++ Server/testing.py
@@ -4,6 +4,7 @@ from ChessValidator import ChessValidator
 from contextlib import contextmanager
 import sys, os
 import inspect
+import argparse
 
 # --------------------
 # For suppressing the output of the functions which are tested
@@ -341,6 +342,75 @@ def testing_chess_validator_move_rook():
     return result
 
 # ---------------------------
+# Testing the knight
+# ---------------------------
+def testing_chess_validator_move_knight():
+    result = ['>> Testing Move Knight']
+    chess_validator = ChessValidator()
+
+    # ------
+    # Check all possible moves
+
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|               |
+3|p   n   p     q|
+2|  p p p   p p p|
+1|r   b   k b n r|
+""")
+    
+    from_string = 'c3'
+    active_player = 0
+
+    to_strings = ['d1', 'b1', 'a2', 'a4', 'b5', 'd5', 'e4', 'e2']
+    count = 1
+    for to_string in to_strings:
+        with suppress_stdout():
+            result.append(test_result(
+                chess_validator.validate_move(
+                board, from_string, to_string, active_player),
+                (f'Drawing knight c3 to {to_string} ({count})'),
+                line_number(),
+                False))
+        count += 1
+
+    # ------
+    # Test moves that shouldn't work
+
+    board = generate_board_from_string("""
+8|R N B Q K B N R|
+7|P P P P P P P P|
+6|               |
+5|     N         |
+4|               |
+3|               |
+2|p p p p p p p p|
+1|r n b q k b n r|
+""")
+    
+    from_string = 'd5'
+    active_player = 0
+
+    to_strings = ['d6', 'e5', 'c5', 'd4', 'h1', 'a5', 'h3', 'f1']
+    count = 1
+    for to_string in to_strings:
+        with suppress_stdout():
+            result.append(test_result(
+                chess_validator.validate_move(
+                board, from_string, to_string, active_player),
+                (f'Drawing knight (invalid) c3 to {to_string} ({count})'),
+                line_number()))
+        count += 1
+
+    # ------
+
+    result.append('> Finished')
+    return result
+
+# ---------------------------
 # Testing the bishop
 # ---------------------------
 def testing_chess_validator_move_bishop():
@@ -604,9 +674,24 @@ def run_test(testing_function, shorten):
             print(r)
     print('')    
 
+def parse_arguments():
+    """
+    Parse the command line arguments if there are any.
+    """
+    parser = argparse.ArgumentParser(description='Test all existing test cases.')
+    # Add the shorten argument
+    parser.add_argument(
+            'shorten',
+            help='Shorten the output to print only the details of failed tests.'
+    )
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    shorten = False
+    args = parse_arguments()
+
+    shorten = True if args.shorten else False
     run_test(testing_chess_validator_move_generic, shorten)
     run_test(testing_chess_validator_move_rook, shorten)
+    run_test(testing_chess_validator_move_knight, shorten)
     run_test(testing_chess_validator_move_bishop, shorten)
 
