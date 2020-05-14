a7bd17ce6cf48a87afa4cbfcec91970d5355e9fc
MichelKrispin <michel.krispin@gmail.com>
2020-05-14 10:51:47 +0200

    Added King test cases

--- Server/testing.py
+++ Server/testing.py
@@ -580,6 +580,9 @@ def testing_chess_validator_move_bishop():
     result.append('> Finished')
     return result
 
+# --------------------
+# Queen
+# --------------------
 def testing_chess_validator_move_queen():
     result = ['>> Testing Move Queen']
     chess_validator = ChessValidator()
@@ -626,6 +629,9 @@ def testing_chess_validator_move_queen():
     result.append('> Finished')
     return result
 
+# --------------------
+# Testing the king
+# --------------------
 def testing_chess_validator_move_king():
     result = ['>> Testing Move King']
     chess_validator = ChessValidator()
@@ -668,10 +674,61 @@ def testing_chess_validator_move_king():
         count += 1
 
     # ------
+    # Then drawing next to the other king (valid)
+    
+    board = generate_board_from_string("""
+8|R N B Q   B N R|
+7|P P P P P P P P|
+6|               |
+5|    K     k    |
+4|               |
+3|               |
+2|p p p p p p p p|
+1|r n b q   b n r|
+""")
+    
+    from_string = 'f5'
+    to_strings = ['e5', 'e6', 'e4']
+    count = 1
+    for to_string in to_strings:
+        result.append(run_individiual_chess_validate(line_number(),chess_validator.validate_move,
+            board, from_string, to_string, active_player,
+            f'Drawing king one step away from enemy king e5 to {to_string} ({count})',
+            Expect.TRUE))
+        count += 1
+
+    # ------
+    # Then drawing next to the other king (invalid)
+    
+    board = generate_board_from_string("""
+8|R N B Q   B N R|
+7|P P P P P P P P|
+6|               |
+5|    K   k      |
+4|               |
+3|               |
+2|p p p p p p p p|
+1|r n b q   b n r|
+""")
+
+    from_string = 'e5'
+    to_strings = ['d5', 'd6', 'd4']
+    count = 1
+    for to_string in to_strings:
+        result.append(run_individiual_chess_validate(line_number(),chess_validator.validate_move,
+            board, from_string, to_string, active_player,
+            f'Drawing king (invalid) next to enemy king e5 to {to_string} ({count})',
+            Expect.FALSE))
+        count += 1
+
+    # ------
 
     result.append('> Finished')
     return result
 
+# --------------------
+# Pawn
+# --------------------
 def testing_chess_validator_move_pawn():
     result = ['>> Testing Move Pawn']
     chess_validator = ChessValidator()
