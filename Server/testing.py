397f4fdeb9d6b63fefd5806c6356f4614eaf924a
MichelKrispin <michel.krispin@gmail.com>
2020-05-18 18:59:29 +0200

    Added tests for new features

--- Server/testing.py
+++ Server/testing.py
@@ -1158,6 +1158,56 @@ def testing_chess_validator_king_can_be_saved():
             line_number(),
             Expect.FALSE))
 
+    # ------
+    # And at the end test for special cases
+
+    boards = []
+    boards.append(generate_board_from_string("""
+8|R N B   K B N R|
+7|P P P P P P P P|
+6|               |
+5|        Q      |
+4|               |
+3|               |
+2|p p p p   p p p|
+1|r n b q k b n r|
+""")) # 0
+    boards.append(generate_board_from_string("""
+8|R N B   K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|               |
+3|               |
+2|p p p p Q p p p|
+1|r n b q k b n r|
+""")) # 1
+    boards.append(generate_board_from_string("""
+8|R N B   K B N R|
+7|P P P P P P P P|
+6|               |
+5|               |
+4|              Q|
+3|               |
+2|p p p p     p p|
+1|r n b q k b n r|
+""")) # 2
+
+    attackers = [[['e', 4]], [['e', 1]], [['h', 3]]]
+    count = 0
+    for board in boards:
+        chess_validator.board = board
+        chess_validator.attackers = attackers[count]
+        with suppress_stdout():
+            result.append(test_result(
+                chess_validator.validate_king_can_be_saved(),
+                f'King can be saved (board {count})',
+                line_number(),
+                Expect.TRUE))
+        count += 1
+
+    # ------
+
     result.append('> Finished')
     return result
 
